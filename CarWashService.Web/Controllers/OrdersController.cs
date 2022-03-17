5377bc1339713773568257bfdd8819799f838d8a
thepicture <85333076+thepicture@users.noreply.github.com>
2022-03-17 06:30:09 +0000

    feat(controller): implement the orders controller

---
+++ CarWashService.Web/Controllers/OrdersController.cs
@@ -0,0 +1,123 @@
+﻿using CarWashService.Web.Models.Entities;
+using CarWashService.Web.Models.Entities.Serialized;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace CarWashService.Web.Controllers
+{
+    public class OrdersController : ApiController
+    {
+        private CarWashBaseEntities db = new CarWashBaseEntities();
+
+        // GET: api/Orders
+        [Authorize(Roles = "Администратор, Сотрудник")]
+        public IHttpActionResult GetOrder()
+        {
+            var orders = db.Order.ToList()
+                .ConvertAll(o => new SerializedOrder(o));
+            return Ok(orders);
+        }
+
+        // GET: api/Orders/5
+        [ResponseType(typeof(Order))]
+        [Authorize(Roles = "Администратор, Сотрудник")]
+        public async Task<IHttpActionResult> GetOrder(int id)
+        {
+            Order order = await db.Order.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(new SerializedOrder(order));
+        }
+
+        // PUT: api/Orders/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutOrder(int id, Order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != order.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(order).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Orders
+        [ResponseType(typeof(Order))]
+        [Authorize(Roles = "Администратор, Сотрудник")]
+        public async Task<IHttpActionResult> PostOrder(Order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Order.Add(order);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi",
+                                  new { id = order.Id },
+                                  new SerializedOrder(order));
+        }
+
+        // DELETE: api/Orders/5
+        [ResponseType(typeof(Order))]
+        public async Task<IHttpActionResult> DeleteOrder(int id)
+        {
+            Order order = await db.Order.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            db.Order.Remove(order);
+            await db.SaveChangesAsync();
+
+            return Ok(order);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool OrderExists(int id)
+        {
+            return db.Order.Count(e => e.Id == id) > 0;
+        }
+    }
+}
