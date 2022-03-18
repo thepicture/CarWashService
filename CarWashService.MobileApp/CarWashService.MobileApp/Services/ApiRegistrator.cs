e8a20efa9d4dc39d2305a4d0cd625dc8cb6d0970
thepicture <85333076+thepicture@users.noreply.github.com>
2022-03-18 08:51:37 +0000

    feat(apiregistrator): add the post method to API to register

--- CarWashService.MobileApp/CarWashService.MobileApp/Services/ApiRegistrator.cs
+++ CarWashService.MobileApp/CarWashService.MobileApp/Services/ApiRegistrator.cs
@@ -1,4 +1,8 @@
 ﻿using CarWashService.MobileApp.Models.Serialized;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace CarWashService.MobileApp.Services
@@ -7,7 +11,38 @@ namespace CarWashService.MobileApp.Services
     {
         public async Task<bool> IsRegisteredAsync(SerializedUser identity)
         {
-            return await Task.FromResult(false);
+            using (WebClient client = new WebClient())
+            {
+                string jsonIdentity = JsonConvert.SerializeObject(identity);
+                byte[] encodedJsonIdentity = Encoding.UTF8
+                    .GetBytes(jsonIdentity);
+                client.BaseAddress = (App.Current as App).BaseUrl;
+                client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+                try
+                {
+                    byte[] response = await client
+                        .UploadDataTaskAsync("api/users/register", encodedJsonIdentity);
+                    return true;
+                }
+                catch (WebException ex)
+                {
+                    if ((ex.Response as HttpWebResponse).StatusCode
+                        == HttpStatusCode.BadRequest)
+                    {
+                        Debug.WriteLine(ex.StackTrace);
+                        throw;
+                    }
+                    else if ((ex.Response as HttpWebResponse).StatusCode
+                        == HttpStatusCode.Conflict)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
         }
     }
 }
